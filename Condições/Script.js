773457a21e4addbf63177e0310ff791f16f79b0f
Renato Alencar <alencar.renato@yahoo.com.br>
2019-10-25 22:02:33 -0300

    Condição Simples

---
+++ Condições/Script.js
@@ -0,0 +1,7 @@
+var vel = 70.5
+console.log(`A Velocidade do Carro é: ${vel} Km/h`)
+if(vel <= 60){
+    console.log(`Voce esta na Velocidade Correta ${vel} Dirija com Segurança`)
+}else{
+    console.log(`Voce Ultrapassou a Velocidade Permitida ${vel}`)
+}
